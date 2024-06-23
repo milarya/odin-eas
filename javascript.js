b10dfdba226bda6e8ccef8d45df9e259fd636d55
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-23 14:09:11 +0200

    feat: Add button to start new grid

--- javascript.js
+++ javascript.js
@@ -1,25 +1,40 @@
 console.log('Javascript loaded');
 
-const GRIDSIZE = 16;
-
-
 const container = document.querySelector('.gridContainer');
 
-// create grid elements
-for (i = 0; i < GRIDSIZE*GRIDSIZE; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.setAttribute('id', i);
-    gridElement.setAttribute('class', 'gridElement');
-    console.log(gridElement);
-    container.appendChild(gridElement);
+// create a grid to play a game
+function createGrid(size) {
+    // delete old grid
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    // calculate + draw new grid
+    const gridSize = size
+    for (i = 0; i < gridSize*gridSize; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.setAttribute('id', i);
+        gridElement.setAttribute('class', 'gridElement');
+        console.log(gridElement);
+        container.appendChild(gridElement);
+    }
+
+    // add event listeners to grid elements for hover darken effect
+    const gridElements = document.querySelectorAll('.gridElement');
+
+    gridElements.forEach((element) => {
+        element.addEventListener('mouseenter', (event) => {
+            console.log(event.target);
+            event.target.setAttribute('class', 'darkened gridElement');
+        })
+    });
 }
 
-// event listeners for hover darken effect
-const gridElements = document.querySelectorAll('.gridElement');
-
-gridElements.forEach((element) => {
-    element.addEventListener('mouseenter', (event) => {
-        console.log(event.target);
-        event.target.setAttribute('class', 'darkened gridElement');
-    })
+// event listener for reset grid
+const btnReset = document.querySelector('.btnReset');
+btnReset.addEventListener('click', () => {
+    createGrid(+prompt('Please enter desired size of grid'));
 });
+
+// create initial grid to start game
+createGrid(16);
