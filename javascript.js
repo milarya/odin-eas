32fe5b93af44a9ef2d08d7add2b24836cff7faee
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-23 13:31:12 +0200

    feat: Add grid elements with rows

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
 console.log('Javascript loaded');
+
+const GRIDSIZE = 16;
+
+
+const container = document.querySelector('.gridContainer');
+
+// create grid elements
+// rows
+for (i = 0; i < GRIDSIZE; i++) {
+    const gridRow = document.createElement('div');
+    gridRow.setAttribute('id', i);
+    gridRow.setAttribute('class', 'gridRow');
+    // columns
+    for (j = 0; j < GRIDSIZE; j++) {
+        const gridElement = document.createElement('div');
+        gridElement.setAttribute('id', (i*GRIDSIZE) + j);
+        gridElement.setAttribute('class', 'gridElement');
+        console.log(gridElement);
+        gridRow.appendChild(gridElement);
+    }
+    container.appendChild(gridRow);
+
+}
