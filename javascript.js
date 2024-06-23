c8db140cf39c322a80920544c32445c5c8cad794
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-23 13:39:06 +0200

    feat: Create grid elements without rows

--- javascript.js
+++ javascript.js
@@ -6,19 +6,10 @@ const GRIDSIZE = 16;
 const container = document.querySelector('.gridContainer');
 
 // create grid elements
-// rows
-for (i = 0; i < GRIDSIZE; i++) {
-    const gridRow = document.createElement('div');
-    gridRow.setAttribute('id', i);
-    gridRow.setAttribute('class', 'gridRow');
-    // columns
-    for (j = 0; j < GRIDSIZE; j++) {
-        const gridElement = document.createElement('div');
-        gridElement.setAttribute('id', (i*GRIDSIZE) + j);
-        gridElement.setAttribute('class', 'gridElement');
-        console.log(gridElement);
-        gridRow.appendChild(gridElement);
-    }
-    container.appendChild(gridRow);
-
+for (i = 0; i < GRIDSIZE*GRIDSIZE; i++) {
+    const gridElement = document.createElement('div');
+    gridElement.setAttribute('id', i);
+    gridElement.setAttribute('class', 'gridElement');
+    console.log(gridElement);
+    container.appendChild(gridElement);
 }
